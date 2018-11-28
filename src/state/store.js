a943195f68a48af19f93f9a94b7021b1ad52f399
starryforce <starryforce@gmail.com>
2018-11-28 18:39:17 +0800

    升级：同步模板最新文件

--- src/state/store.js
+++ src/state/store.js
@@ -13,6 +13,9 @@ const store = new Vuex.Store({
       modules: ['setting'],
     }).plugin,
   ],
+  // Enable strict mode in development to get a warning
+  // when mutating state outside of a mutation.
+  // https://vuex.vuejs.org/guide/strict.html
   strict: process.env.NODE_ENV !== 'production',
 })
 
