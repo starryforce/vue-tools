8077e371a8e934b95b44caf7dda9705c783da878
philip <bakujin@qq.com>
2018-11-28 13:30:43 +0800

    拉取储值活动信息

--- src/router/views/AddCredit.vue
+++ src/router/views/AddCredit.vue
@@ -1,4 +1,5 @@
 <script>
+/* eslint-disable */
 import Layout from '@layouts/WithToolBar'
 
 export default {
@@ -18,9 +19,13 @@ export default {
     return {
       value: 5,
       payment: 'cash',
+      list: [],
     }
   },
-  created() {},
+  async created() {
+    var res = await this.$api.order.getRecharge()
+    this.list = res.data
+  },
   methods: {},
 }
 </script>
@@ -29,21 +34,11 @@ export default {
   <Layout>
     <VCard>
       <VCardTitle>
-        <VItemGroup
-          v-model="value"
-          mandatory
-        >
+        <VItemGroup v-model="value" mandatory>
           <VSubheader>充值金额：</VSubheader>
-          <VContainer
-            fluid
-            grid-list-md
-          >
+          <VContainer fluid grid-list-md>
             <VLayout wrap>
-              <VFlex
-                v-for="i in 6"
-                :key="i"
-                xs6
-              >
+              <VFlex v-for="i in list" :key="i.id" xs6>
                 <VItem>
                   <VBtn
                     slot-scope="{ active, toggle }"
@@ -51,42 +46,21 @@ export default {
                     :class="$style.options"
                     block
                     @click="toggle"
-                  >
-                    {{ i*1000 }}元
-                  </VBtn>
+                  >{{ i.activityPrice }}元</VBtn>
                 </VItem>
               </VFlex>
             </VLayout>
           </VContainer>
-          <VRadioGroup
-            v-model="payment"
-            :mandatory="false"
-          >
-            <VRadio
-              label="微信支付"
-              value="weixin"
-            />
-            <VRadio
-              label="支付宝支付"
-              value="alipay"
-            />
-            <VRadio
-              label="现金支付"
-              value="cash"
-            />
+          <div v-for="item in list" :key="item.id">{{item.activityName}}</div>
+          <VRadioGroup v-model="payment" :mandatory="false">
+            <VRadio label="微信支付" value="weixin"/>
+            <VRadio label="支付宝支付" value="alipay"/>
+            <VRadio label="现金支付" value="cash"/>
           </VRadioGroup>
         </VItemGroup>
       </VCardTitle>
     </VCard>
-    <VBtn
-      :class="$style.button"
-      block
-      color="primary"
-      dark
-      fixed
-    >
-      充值
-    </VBtn>
+    <VBtn :class="$style.button" block color="primary" dark fixed>充值</VBtn>
   </Layout>
 </template>
 
