3e94126a6947e89aedf603956b1f68bef366f83c
starryforce <starryforce@gmail.com>
2018-11-30 18:45:18 +0800

    新增：雇员接口注释

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,5 +1,30 @@
 import request from '@utils/request'
 
+/**
+ *
+ * @typedef {Object} GuideEmployeeKpiDetail - 对象属性
+ * @property {string} activationCount - 激活人数
+ * @property {string} customerCount - 会员数
+ * @property {string} repurchaseCount - 复购人数
+ * @property {string} currentRepurchaseCount - 当前复购人数
+ * @property {number} currentCustomerCount - 当前会员数
+ * @property {number} currentSalesAmount - 当前销售额
+ * @property {string} currentActivationCount - 当前激活人数
+ * @property {string} salesAmount - 销售额
+ * @property {number} month - 月份
+ *
+ * @typedef {Object} GuideEmployeeDetail - 客户信息
+ * @property {number} totalAssignAmount - 总改价额度
+ * @property {string} role - 角色
+ * @property {string} storeName - 所属店铺姓名
+ * @property {string} storeId - 所属店铺id
+ * @property {number} lastAssignAmount - 剩余改价额度
+ * @property {string} employeeId - 销售员id
+ * @property {GuideEmployeeKpiDetail} kpiDetail - Kpi
+ * @property {string} employeeName - 姓名
+ *
+ * @returns {GuideEmployeeDetail} employeeInfo - 会员信息
+ */
 export const getGuideEmployeeDetail = () => {
   return request({
     url: '/GuideEmployee/GetGuideEmployeeDetail',
