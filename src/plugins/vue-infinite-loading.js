e45f19c0346e899f40a466926dbd5716af12bc28
starryforce <starryforce@gmail.com>
2018-12-11 15:24:13 +0800

    修改：变更底部提示

--- src/plugins/vue-infinite-loading.js
+++ src/plugins/vue-infinite-loading.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import InfiniteLoading from 'vue-infinite-loading'
+import NoMore from '@components/NoMore'
 
 Vue.use(InfiniteLoading, {
-  /* 配置 */
+  slots: {
+    // keep default styles
+    noResults: '没有数据',
+    // remove default styles
+    noMore: NoMore,
+  },
 })
